fee354d7f58e121d8d4582f8444d560beef8e51a
Hetkumar Prajapati <74864054+prajapatihet@users.noreply.github.com>
2024-03-23 10:54:51 +0530

    Time: 547 ms (35.67%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,1 +1,6 @@
-select if(count(salary)=0,null,salary) as SecondHighestSalary from (select salary,dense_rank() over (order by salary desc) as dnk from Employee) dnkemp where dnkemp.dnk=2;
+# select if(count(salary)=0,null,salary) as SecondHighestSalary from (select salary,dense_rank() over (order by salary desc) as dnk from Employee) dnkemp where dnkemp.dnk=2;
+
+with hs as(
+select max(salary) as mx from Employee
+)
+select max(salary) as SecondHighestSalary from Employee where salary<(select hs.mx from hs);
