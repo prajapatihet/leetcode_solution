f21cdac9e137fd2d7ee3a7d71a867c2b95960d05
Hetkumar Prajapati <74864054+prajapatihet@users.noreply.github.com>
2024-03-25 10:49:30 +0530

    Time: 2 ms (84.78%), Space: 46.3 MB (23.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length==1){
-            return;
-        }
-        
-        int nonZero=0;
-        
+        int nonZero=0;        
         for(int i=0;i<nums.length;i++){
             if (nums[i]!=0){
-                nums[nonZero]=nums[i];
-                nonZero++;
+                nums[nonZero++]=nums[i];
             }
         }
         
         while(nonZero<nums.length){
-            nums[nonZero]=0;
-            nonZero++;
+            nums[nonZero++]=0;
         }
     }
 }
