88f95da843789fdce1ba48b411974840357399a0
Hetkumar Prajapati <74864054+prajapatihet@users.noreply.github.com>
2024-03-25 15:51:48 +0530

    Time: 30 ms (94.07%), Space: 16.5 MB (97.91%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,13 +1,16 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
         s = s.lstrip()
+
         if not s:
             return 0
+
         sign = 1
-        if s[0] == '-':
-            sign = -1
-            s = s[1:]
-        elif s[0] == '+':
+        if s[0] in ('-', '+'):
+            sign = -1 if s[0] == '-' else 1
             s = s[1:]
 
         result = 0
@@ -18,9 +21,6 @@ class Solution:
                 break
 
         result *= sign
-
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
         if result > INT_MAX:
             return INT_MAX
         elif result < INT_MIN:
